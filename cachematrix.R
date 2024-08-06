c7270f854ba4ff192f1d977c53c635d6ef89e17e
kentxie <71677073+kentxie@users.noreply.github.com>
2024-08-05 22:12:17 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinvert <- function(invert) i <<- invert
+        getinvert <- function() i
+        list(set = set,
+             get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        i <- x$getinvert()
+        if(!is.null(i)){
+                print('getting cached data')
+                return(i)
+        }
+        matrix <- x$get()
+        i <- solve(matrix)
+        x$setinvert(i)
+        i
         ## Return a matrix that is the inverse of 'x'
 }
+
